c27cd24b3747c5a12d93e26b02a95e6c71cdbc0d
Kidoly <58863423+Kidoly@users.noreply.github.com>
2022-12-07 11:52:51 +0100

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -10,20 +10,22 @@ def tictactoe():
     print("7|8|9")
     print("4|5|6")
     print("1|2|3")
+    player = ['0', 'X']
+    player[0] = str(input("Donner le pseudo du premier joueur:"))
+    player[1] = str(input("Donner le pseudo du deuxième joueur:"))
     while True:
-        for i in formes:
+        for i in (0, 1):
             nb += 1
-            player = i
-            position = str(input(f"Joueur {player} saisissez où placer votre signe:"))
+            position = str(input(f"Joueur {player[i]} saisissez où placer votre signe:"))
             while int(position) > 9 or int(position) < 0 or lesPositions[position] != ' ':
-                position = str(input(f'Joueur {player} resaisissez où placer votre signe car cette emplacement est déjà pris:'))
-            lesPositions[position] = i
+                position = str(input(f'Joueur {player[i]} resaisissez où placer votre signe car cette emplacement est déjà pris:'))
+            lesPositions[position] = formes[i]
             print(lesPositions["7"], "|", lesPositions["8"], "|", lesPositions["9"], "           7|8|9")
             print(lesPositions["4"], "|", lesPositions["5"], "|", lesPositions["6"], "           4|5|6")
             print(lesPositions["1"], "|", lesPositions["2"], "|", lesPositions["3"], "           1|2|3")
             for j in range(8):
-                if lesPositions[victoire[j][0]] == i and lesPositions[victoire[j][1]] == i and lesPositions[victoire[j][2]] == i:
-                    return "Le jouer jouant les", i, "a gagné"
+                if lesPositions[victoire[j][0]] == formes[i] and lesPositions[victoire[j][1]] == formes[i] and lesPositions[victoire[j][2]] == formes[i]:
+                    return "Le jouer jouant les", player[i], "a gagné"
                 elif nb > 8:
                     return "match nul"
 print(tictactoe())
